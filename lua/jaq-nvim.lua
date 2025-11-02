ae4bb1377bf7585756fbc1489b3a83d97f616f4d
Tommischiri <tommischiri@gmail.com>
2025-11-02 13:35:46 +0100

    s

--- lua/jaq-nvim.lua
+++ lua/jaq-nvim.lua
@@ -112,11 +112,6 @@ local function float(cmd)
 end
 
 function M.float_call()
-
-    if config.behavior.autosave then
-        vim.cmd("silent write")
-    end
-
     local dim = dimensions(config)
 
     -- Create the floating buffer and window
